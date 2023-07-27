d9c2a620cdbc469aad1925e2457540e2f4ee3b75
soumen-pradhan <sam.thepradhans@gmail.com>
2023-07-27 20:30:33 +0530

    added post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,17 +1,54 @@
-import { NextPage } from "next";
+import { GetServerSidePropsContext, InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
+import { Layout } from "~/components/layout";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
+import { serverHelper } from "~/server/helpers";
+import { api } from "~/utils/api";
 
-const SinglePostPage: NextPage = () => {
+const SinglePostPage = (
+    props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
+    const { id } = props;
+
+    const { data, isLoading } = api.posts.getById
+        .useQuery({ id });
+
+    if (isLoading) return <LoadingPage />;
+    if (!data) return <div>404</div>;
+
     return (
         <>
             <Head>
-                <title>Post</title>
+                <title>{`${data.post.content} @${data.author.username}`}</title>
             </Head>
-            <main className="flex justify-center">
-                <div>Post View</div>
-            </main>
+            <Layout>
+                <PostView {...data} />
+            </Layout>
         </>
     );
 };
+
+export const getServerSideProps = async (
+    context: GetServerSidePropsContext<{ id: string }>
+) => {
+
+    const helpers = serverHelper();
+
+    const id = context.params?.id;
+    if (typeof id !== "string") {
+        throw new Error("No slug");
+    }
+
+    console.log(id);
+
+    await helpers.posts.getById.prefetch({ id });
+    return {
+        props: {
+            trpcState: helpers.dehydrate(),
+            id
+        }
+    }
+}
 
 export default SinglePostPage;
